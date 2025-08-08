4312321ca42136cf111baffd64c34d17337e0318
SINDHU TENAGUNDLAM PRASANNA KUMAR <109784022+SinSuhas@users.noreply.github.com>
2025-08-07 21:43:32 -0700

    Update app.py public ip

--- frontend/app.py
+++ frontend/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configure the API endpoint URL, defaulting to localhost if not specified in .env
-API_URL = os.getenv("API_URL", "http://3.129.90.172:8000")
+API_URL = os.getenv("API_URL", "http://3.128.28.6:8000")
 
 def main():
     """
@@ -101,4 +101,5 @@ def main():
 
 if __name__ == "__main__":
     
+
     main()
